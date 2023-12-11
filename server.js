6a2713c82c239bb88be2c2947f43f0d52a5f003b
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-12-11 22:47:05 +0200

    add: new port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 4443;
 
 // Зчитайте сертифікат та приватний ключ
 const privateKey = fs.readFileSync('private-key.pem', 'utf8');
